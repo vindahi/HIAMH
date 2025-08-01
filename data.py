60362b459ba880ea335b37e58de5e0ddfb6f8f7a
FanFan <140507577+vindahi@users.noreply.github.com>
2025-08-01 10:41:01 +0800

    Update data.py

--- data.py
+++ data.py
@@ -13,46 +13,25 @@ dataset_tuple = namedtuple('dataset_tuple', ['I_tr', 'T_tr', 'L_tr',
 paths = {
     'flickr': ROOT + 'mir_cnn_twt.mat',
     'nuswide': ROOT + 'nus_cnn_twt.mat',
-    'coco': ROOT + 'coco_cnn_twt.mat',
-    'wiki': '/home/user3/zrfan/mvf/Data/wiki_data_all.mat'
+    'coco': ROOT + 'coco_cnn_twt.mat'
 }
 
-# def load_data(DATANAME):
-#     data = sio.loadmat(paths[DATANAME])
-#     I_tr = data['I_tr'][:]
-#     T_tr = data['T_tr'][:]
-#     L_tr = data['L_tr'][:]
-#     I_db = data['I_db'][:]
-#     T_db = data['T_db'][:]
-#     L_db = data['L_db'][:]
-#     I_te = data['I_te'][:]
-#     T_te = data['T_te'][:]
-#     L_te = data['L_te'][:]
-#     return dataset_tuple(I_tr=I_tr, T_tr=T_tr, L_tr=L_tr,
-#                          I_db=I_db, T_db=T_db, L_db=L_db,
-#                          I_te=I_te, T_te=T_te, L_te=L_te)
-
-
-
-
 def load_data(DATANAME):
-    data = h5py.File(paths[DATANAME], 'r')
-
-    I_tr = data['I_tr'][:].T
-    T_tr = data['T_tr'][:].T
-    L_tr = data['L_tr'][:].T
-
-    I_db = data['I_db'][:].T
-    T_db = data['T_db'][:].T
-    L_db = data['L_db'][:].T
-
-    I_te = data['I_te'][:].T
-    T_te = data['T_te'][:].T
-    L_te = data['L_te'][:].T
-
+    data = sio.loadmat(paths[DATANAME])
+    I_tr = data['I_tr'][:]
+    T_tr = data['T_tr'][:]
+    L_tr = data['L_tr'][:]
+    I_db = data['I_db'][:]
+    T_db = data['T_db'][:]
+    L_db = data['L_db'][:]
+    I_te = data['I_te'][:]
+    T_te = data['T_te'][:]
+    L_te = data['L_te'][:]
     return dataset_tuple(I_tr=I_tr, T_tr=T_tr, L_tr=L_tr,
                          I_db=I_db, T_db=T_db, L_db=L_db,
                          I_te=I_te, T_te=T_te, L_te=L_te)
+
+
 
 class my_dataset(data.Dataset):
     def __init__(self, img_feature, txt_feature, label, **kwargs):
